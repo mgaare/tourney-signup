54e5f5e5158117361e8c0db4500a4f6445a53768
Michael Gaare <michael.gaare@gmail.com>
2013-03-16 16:04:56 +0000

    view for signup

---
+++ views/signup.php
@@ -0,0 +1,20 @@
+<?php
+include_protection(__FILE__);
+?>
+<h1>Sign up for the next SWL Tournament</h1>
+
+<p>Taking place <?php echo date("F j, G:i", $event['time']); ?></p>
+
+<form method="post">
+
+<h2>Which modes are you playing in?</h2>
+
+<?php 
+
+foreach ($events as $event) {
+		
+}
+
+?>
+
+</form>
