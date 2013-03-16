169da1c787867f8cb33a5239a2a20956dd6fd6b8
Michael Gaare <michael.gaare@gmail.com>
2013-03-16 16:04:45 +0000

    Added include protection function

--- lib/util.php
+++ lib/util.php
@@ -12,3 +12,12 @@ function array_filter_search($array, $key, $val) {
 function array_first($array) {
 	return array_pop(array_reverse($array));
 }
+
+function include_protection($file) {
+	global $include_protection;
+	
+	if (!(isset($include_protection) && $include_protection)) {
+		error_log('Attempt to view template directly: ' . $file);
+		die();
+	}
+}
