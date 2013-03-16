8034cffd7521e013167a03ddfaaca84cc1687440
Michael Gaare <michael.gaare@gmail.com>
2013-03-16 23:18:47 +0000

    Signup form done

--- views/signup.php
+++ views/signup.php
@@ -10,11 +10,46 @@ include_protection(__FILE__);
 <h2>Which modes are you playing in?</h2>
 
 <?php 
+$snippet = function(&$val) {
+	$ret = "<p><label for='{$val['name']}-checkbox'>{$val['name']}"
+		. "</label>"
+		. "<input type='checkbox' name=\"mode[{$val['id']}]['signup']\""  
+		. " id='" . $val['name'] . "-checkbox'";
+	if (isset($val['signup']) && $val['signup']) {
+		$ret .= " checked";
+	}
+	$ret .= ">";
+	if ($val['team_mode'] == 1) {
+		$ret .= "<p><label for ='{$val['name']}-team'>Team name: "
+			. "<input type='text' name=\"mode[{$val['id']}]['team']\""
+			. " id='{$val['name']}-team'";
+		if (isset($val['team']) && !empty($val['team'])) {
+			$ret .= " value='{$val['team']}'";
+		}
+		$ret .= ">";
+	}
+	return $ret;
+};
 
-foreach ($events as $event) {
-		
-}
-
+echo mapcat($snippet, $modes);
 ?>
 
+<h2>What Maps Do You Want?</h2>
+
+<?php
+
+$snippet = function(&$mode) {
+	$ret = "<p>Map for {$mode['name']} All v All match: "
+		 . "<select name=\"vote['{$mode['name']}']['all_v_all']\">"
+		 . "<option value='' selected>No Vote</option>";
+	return $ret .= mapcat($showMap, $mode['maps']);
+};
+
+$showMap = function(&$map) {
+	return "<option value='{$map['id']}>{$map['name']}</option>";
+};
+
+echo mapcat($snippet, $modes);
+?>
+<input type="Submit" value="Sign Up">
 </form>
