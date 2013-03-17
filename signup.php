404efbe83360a7139bceb24e54bcc85194a2a8a9
Michael Gaare <michael.gaare@gmail.com>
2013-03-17 01:05:35 +0000

    Getting pretty close to the signups controller being done

--- signup.php
+++ signup.php
@@ -1,5 +1,7 @@
 <?php
 
+// This is, for most intents and purposes, a 'controller'
+
 require_once('./lib/base.php');
 
 function log_in() {
@@ -58,13 +60,38 @@ function log_in_error($username, $password) {
 }
 
 function sign_up() {
-	$sign_up = ModelStore::getInstance('Signup');
-	$mode = ModelStore::getInstance('Mode');
-	$map = ModelStore::getInstance('Map');
-	$event = ModelStore::getInstance('Event');
+		
+}
+
+function sign_up_form($message = false) {
+	// prepare for some of the ugliest code in this whole app
+	$signup = new Signup();
+	$mode = new Mode();
+	$map = new Map();
+	$event = new Event();
+	$user = new User();
+	$templateSnippet = new TemplateSnippet();
+	$template = new UserTemplate();
 	
-	
-	
+	$current_event = $event->getCurrent();
+	// gets the modes that are being played in this event, and then attaches
+	// the corresponding maps to them - this is perhaps the only redeeming
+	// code in the entire function here
+	$modes = map(function($mode_val) use($map) {
+					return $mode_val['maps'] = $map->getForMode($mode_val);
+				 }, $mode->getForEvent($current_event));
+	$current_user = $user->getLoggedIn();
+	$current_signup = $signup->getCurrentForUser($current_user, 
+												 $current_event);
+	$view_vars = array('event' => $current_event, 'modes' => $modes, 
+					   'user' => $current_user, 'signup' => $current_signup);
+					   
+	$templateSnippet->setTemplateFile('signup.php');
+	$content = $templateSnippet->render($view_vars);
+	if ($message) {
+		$content = "<h2 class='error'>{$message}</h2>" . $content;
+	}
+	echo $template->render($content);
 }
 
 $user = new User();
