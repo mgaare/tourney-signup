30b9681aae08811cd7c70b320d4f629f29445b14
Michael Gaare <michael.gaare@gmail.com>
2013-03-31 18:45:06 +0000

    event edit function; events need to have relatd modes too

--- admin/events.php
+++ admin/events.php
@@ -5,6 +5,9 @@ require_once('../lib/base.php');
 function events() {
 	if (isset($_GET['action']) && !empty($_GET['action'])) {
 		switch ($_GET['action']) {
+			case 'view':
+				view_event();
+				break;
 			case 'edit':
 				edit_event();
 				break;
@@ -24,6 +27,7 @@ function events() {
 
 function event_index() {
 	$event = new Event();
+	$mode = new Mode();
 	$template = new AdminTemplate();
 	$templateSnippet = new TemplateSnippet();
 
@@ -31,8 +35,18 @@ function event_index() {
 								 array('order' => 
 								 	   array('by' => 'time', 
 								 	   		 'dir' => 'desc')));
+	$events = map(function($event) use ($mode) {
+			$event['modes'] = $mode->getForEvent($event);
+			return $event;
+		}, $event->findSimple(false, 
+			array('order' => 
+				array('by' => 'time', 
+					  'dir' => 'desc'))));
+	$modes = $mode->findSimple();
 	$current_event = $event->getCurrent();
-	$view_vars = array('events' => $events, 'current_event' => $current_event);
+	$view_vars = array('events' => $events, 
+					   'current_event' => $current_event,
+					   'modes' => $modes);
 	$templateSnippet->setTemplateFile('../views/admin/event.php');
 	$content = $templateSnippet->render($view_vars);
 	echo $template->render($content);
